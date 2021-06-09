5ada48e1b9585b55aef0fbd6fe0fa2acbeffefa8
JoySureshkumar1 <84517105+Joy-sureshkumar@users.noreply.github.com>
2021-06-09 16:40:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('button').addEventListener('click', check)
+let age = 0
+let date = 'Saturday'
+
+function check () {
+  age = document.getElementById('day').value
+  age = parseInt(age)
+
+  date = document.getElementById('age').value
+
+  if ((date === 'Wednesday' || date === 'Saturday') || (age > 13 || age < 20)){ 
+    document.getElementById('answer').innerHTML = 'student discount.'
+      } else {
+    document.getElementById('answer').innerHTML = 'regular pricing.'
+  }
+}
